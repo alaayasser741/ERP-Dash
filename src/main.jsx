6517900f116380514c473c1a48241d0651d0fc00
Alaa Yasser <alaayasser2018@gmail.com>
2024-10-17 20:02:44 +0300

    Update dependencies and add react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { CssBaseline } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins",
+  },
+});
+
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <StrictMode>
+      <CssBaseline />
+      <App />
+    </StrictMode>
+  </ThemeProvider>
+);
